b09f7470995b25153037159f50807e9a95a61cb3
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-08-08 17:51:11 -0400

    PEel away Gatsby starter defaults and make something not out of touch with reality.

--- src/components/layout.js
+++ src/components/layout.js
@@ -26,21 +26,16 @@ function Layout({ children }) {
       <footer className="bg-blue-700">
         <nav className="flex justify-between max-w-4xl mx-auto p-4 md:p-8 text-sm">
           <p className="text-white">
-            Created by{" "}
-            <a
-              href="https://taylorbryant.blog"
-              className="font-bold no-underline text-white"
-            >
-              Taylor Bryant
-            </a>
+            Design by{" "}
+              Artist
           </p>
 
           <p>
             <a
-              href="https://github.com/taylorbryant/gatsby-starter-tailwind"
+              href="https://github.com/yhtmmoney/youhavetoomuchmoney.com"
               className="font-bold no-underline text-white"
             >
-              GitHub
+              GitHub (though why you’d want to?)
             </a>
           </p>
         </nav>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import catAndHumanIllustration from "../images/cat-and-human-illustration.svg";
 
 function IndexPage() {
   return (
@@ -13,26 +12,8 @@ function IndexPage() {
       />
 
       <section className="text-center">
-        <img
-          src={catAndHumanIllustration}
-          className="block mx-auto w-1/2"
-          alt="Cat and human sitting on a couch"
-        />
-
-        <h2 className="bg-yellow-400 text-2xl font-bold inline-block my-8 p-3">
-          Hey there! Welcome to your first Gatsby site.
-        </h2>
-
-        <p className="leading-loose">
-          This is a barebones starter for Gatsby styled using{" "}
-          <a
-            href="https://tailwindcss.com/"
-            className="font-bold no-underline text-gray-900"
-          >
-            Tailwind
-          </a>
-          , a utility-first CSS framework.
-        </p>
+        <h1 className="text-5xl">Coming 🥄:</h1>
+        <h2>A Guidebook to Professional Managerial Class Treason</h2>
       </section>
     </Layout>
   );
