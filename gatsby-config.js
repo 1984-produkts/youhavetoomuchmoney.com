d2a544b66d3701ae82da27489838a37126ff9682
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-08-09 15:12:28 -0400

    Add media player.

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,14 +10,37 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-145308243-1",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
-        // Setting this parameter is optional
         anonymize: true,
-        // Setting this parameter is also optional
         respectDNT: true,
-        // Avoids sending pageview hits from custom paths
         exclude: ["/preview/**", "/do-not-track/me/too/"]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+        episodes: require.resolve("./src/components/episodes-layout.js"),
+        default: require.resolve("./src/components/default-page-layout.js"),
+        gatsbyRemarkPlugins: [
+          {
+            resolve: "gatsby-remark-audio",
+            options: {
+              preload: "auto",
+              loop: false,
+              controls: true,
+              muted: false,
+              autoplay: false
+            }
+          },
+        ]
+      }
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+        ]
       }
     },
     `gatsby-plugin-postcss`,
@@ -43,13 +66,5 @@ module.exports = {
         path: `${__dirname}/content/episodes/`
       }
     },
-    {
-      resolve: `gatsby-plugin-mdx`,
-      options: {
-        extensions: [`.mdx`, `.md`],
-        episodes: require.resolve("./src/components/episodes-layout.js"),
-        default: require.resolve("./src/components/default-page-layout.js")
-      }
-    }
   ]
 };
